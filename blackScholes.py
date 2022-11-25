b6d843c24fc5d287291fd0adb4d3c6f26499359d
jagmeetsingh <jagmeet.r9@gmail.com>
2022-11-25 22:33:31 +0530

    test_bs_commit

---
+++ blackScholes.py
@@ -0,0 +1,32 @@
+
+from scipy import log, sqrt, stats
+from numpy import exp
+
+
+def blackscholes_call(S, E, T, rf, sigma):
+    # first we have to calculate d1 and d2 parameters
+    d1 = (log(S / E) + (rf + sigma * sigma / 2.0) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    # print(d1)
+    # print(d2)
+    # we need N(x) normal distribution function
+    return S * stats.norm.cdf(d1) - E * exp(-rf * T) * stats.norm.cdf(d2)
+
+
+def blackscholes_put(S, E, T, rf, sigma):
+    # first we have to calculate d1 and d2 parameters
+    d1 = (log(S / E) + (rf + sigma * sigma / 2.0) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    # we need N(x) normal distribution function
+    return -S * stats.norm.cdf(-d1) + E * exp(-rf * T) * stats.norm.cdf(-d2)
+
+
+if __name__ == "__main__":
+    S0 = 100  # underlying stock price at t=0
+    E = 100  # strike price
+    T = 1  # expiry 1=1year=365days
+    rf = 0.05  # risk-free rate
+    sigma = 0.2  # volatility of the underlying stock
+
+    print("Call option price according to Black-Scholes model: ", blackscholes_call(S0, E, T, rf, sigma))
+    print("Put option price according to Black-Scholes model: ", blackscholes_put(S0, E, T, rf, sigma))
